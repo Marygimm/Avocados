e249d902546468de43bea9df1a57b7ba0714eba9
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-25 14:32:06 +0000

    create project and add assets

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Mary Moreira on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Mary Moreira on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
